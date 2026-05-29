34820e807b2c519cc7997e23e8312204a546f168
Dostonnnn <davronovvv707@gmail.com>
2026-05-29 22:36:52 +0500

    done

---
+++ js/login.js
@@ -0,0 +1,59 @@
+const API = "https://fakestoreapi.com/auth/login";
+const form = document.querySelector(".form");
+const submit = document.querySelector(".submit");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = document.querySelector(".username").value.trim();
+  const parol = document.querySelector(".password").value.trim();
+  if (!username || !parol) {
+    Toastify({
+      text: "Invalid username or password",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #8c092c, #600c0f)",
+      },
+      onClick: function () {},
+    }).showToast();
+  } else {
+    const userInfo = { username: username, password: parol };
+    fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.token) {
+          const { token } = data;
+          localStorage.setItem("token", token);
+          window.location.href = "../html/main.html";
+        }
+      })
+      .catch((error) => {
+        Toastify({
+          text: "Invalid username or password",
+          duration: 3000,
+          destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background: "linear-gradient(to right, #8c092c, #600c0f)",
+          },
+          onClick: function () {},
+        }).showToast();
+        throw new Error(error);
+      });
+  }
+});
---
+++ js/main.js
@@ -0,0 +1,108 @@
+"use strict";
+const api = `https://fakestoreapi.com/products`;
+const tbody = document.querySelector("tbody");
+let products;
+function getProduct(url) {
+  fetch(url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      products = data;
+      showProducts(products);
+    })
+    .catch((error) => {
+      Toastify({
+        text: "Invalid username or password",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #8c092c, #600c0f)",
+        },
+        onClick: function () {},
+      }).showToast();
+
+      throw new Error(error);
+    });
+}
+
+getProduct(api);
+
+function showProducts(data) {
+  data.forEach((element) => {
+    const { description, title, category, id, image, price } = element;
+    tbody.innerHTML += `<tr>
+                <td>${id}</td>
+                <td>
+                  <img
+                    src="${image}"
+                    alt="${description}"
+                    class="table-img"
+                  />
+                </td>
+                <td>${title}</td>
+                <td>${category}</td>
+                <td class="table-price">${price}</td>
+                <td>
+                  <div class="btns">
+                    <button class="btn-view">View</button>
+                    <button class="btn-edit">Edit</button>
+                    <button class="btn-delete">Delete</button>
+                  </div>
+                </td>
+              </tr>`;
+  });
+}
+
+const form = document.querySelector(".search-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const image = form["image"].value.trim();
+  const title = form["title"].value.trim();
+  const price = form["price"].value.trim();
+  const category = form["category"].value.trim();
+  const description = form["description"].value.trim();
+
+  const product = {
+    image: image,
+    title: title,
+    price: price,
+    category: category,
+    description: description,
+  };
+
+  fetch(api, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      Toastify({
+        text: "Added Successfully",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #4ade80, #4ade80)",
+        },
+        onClick: function () {},
+      }).showToast();
+      console.log(data);
+    });
+});
+
+function deleteProduct(id) {
+  console.log(id);
+}
